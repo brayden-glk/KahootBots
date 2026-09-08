c44b05227bf89e8556bb8795ce8110c59338f3a5
brayden-glk <braydengohlk@gmail.com>
2026-09-08 17:25:33 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from playwright.sync_api import sync_playwright
+import subprocess
+import sys
+import os
+
+NumberOfBots = 0
+KAHOOT_ID = 338758
+
+for i in range(999):
+    try: 
+        os.remove(f"scraper{i}.py")
+    except FileNotFoundError:
+        pass
+
+for i in range(NumberOfBots):
+    with open(f"scraper{i}.py", "w") as f:
+            f.write(
+rf'''
+from playwright.sync_api import sync_playwright
+import time
+BotNumber = {i}
+KAHOOT_ID = {KAHOOT_ID}
+print(rf"Running scraper{i}.py")
+with sync_playwright() as pw:
+    browser = pw.chromium.launch(channel="chrome")
+    page = browser.new_page()
+    page.goto("https://kahoot.it/")
+
+    textbox = page.get_by_role("textbox")
+    textbox.fill(f"{KAHOOT_ID}")
+    page.get_by_role("button", name="Join").click()
+    time.sleep(2)
+
+    username = page.get_by_role("textbox")
+    username.fill(f"scraper{i}")
+    page.get_by_role("button", name="OK, go!").click()
+
+    print(f"BotNumer{i} joined")
+
+    time.sleep(30)
+
+    browser.close()''')
+
+for i in range(NumberOfBots):
+    subprocess.Popen([f"{sys.executable}", f"scraper{i}.py"])
+
+
