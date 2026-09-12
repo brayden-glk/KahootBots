be703e412ded21753db4a07526c6000006ee5ab7
brayden-glk <braydengohlk@gmail.com>
2026-09-12 14:53:47 +0800

    Switch to Multiprocessing, no creation of python files

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
-import subprocess
-import sys
-import os
+import multiprocessing
 
-NUMBER_OF_BOTS = 40
-KAHOOT_ID = 151364
+NUMBER_OF_BOTS = 30 
+KAHOOT_ID = 729004
 
-def main():
-    for i in range(999):
-        try: 
-            os.remove(f"scraper{i}.py")
-        except FileNotFoundError:
-            pass
-
-    for i in range(NUMBER_OF_BOTS):
-        with open(f"scraper{i}.py", "w") as f:
-                f.write(
-    rf'''
+def scraper(bot_number):
     from playwright.sync_api import sync_playwright
     import time
-    BotNumber = {i}
-    KAHOOT_ID = {KAHOOT_ID}
-    print(rf"Running scraper{i}.py")
+    print(rf"Running Bot Number {bot_number}")
     with sync_playwright() as pw:
         browser = pw.chromium.launch(channel="chrome")
         page = browser.new_page()
@@ -32,17 +18,25 @@ def main():
         time.sleep(2)
 
         username = page.get_by_role("textbox")
-        username.fill(f"scraper{i}")
+        username.fill(f"scraper{bot_number}")
         page.get_by_role("button", name="OK, go!").click()
 
-        print(f"BotNumber{i} joined")
+        print(f"Bot Number {bot_number} joined")
 
         time.sleep(30)
 
-        browser.close()''')
+        browser.close()
+
+def main():
+    processes = []
 
     for i in range(NUMBER_OF_BOTS):
-        subprocess.Popen([f"{sys.executable}", f"scraper{i}.py"])
+        p = multiprocessing.Process(target=scraper, args=(i,))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
 
 if __name__ == "__main__":
     main()
